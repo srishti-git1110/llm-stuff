9dac02d1e30626c81ba0bbea910804baf846b3c7
srishti-git1110 <srishtigureja1110@gmail.com>
2025-12-14 00:11:19 +0530

    naive beam search

--- decoding/decoding.py
+++ decoding/decoding.py
@@ -78,3 +78,57 @@ def temp_top_p_top_k(logits: torch.Tensor, p: Optional[float]=None, k: Optional[
         if p <= 0.0 or p > 1.0:
             raise ValueError("p must be in the range (0.0, 1.0].")
         return top_p(scaled_logits, p)
+
+
+def beam_search(
+    model: torch.nn.Module,
+    tokenizer,
+    prompts: list[str], # list of input prompts
+    max_tokens: int,
+    k: int,
+    alpha: float = 0.7,
+):
+    model.eval()
+    with torch.no_grad():
+        inputs = tokenizer(prompts, return_tensors="pt", padding=True)
+        input_ids = inputs["input_ids"].unsqueeze(1).repeat(1, k, 1)  # (bs, k, seq_len)
+        # attention_mask = inputs["attention_mask"].unsqueeze(1).repeat(1, k, 1)
+
+        bs, _, sl = input_ids.size()
+
+        beam_scores = torch.zeros((bs, k), device=input_ids.device)
+        beam_scores[:, 1:] = -1e9 # only the 1st beam is considered initially
+        finished = torch.zeros((bs, k), dtype=torch.bool, device=input_ids.device)
+
+        # treat each beam as a batch example
+        input_ids = input_ids.view(bs*k, sl)
+        # attention_mask = attention_mask.view(bs*k, sl)
+
+        for _ in range(max_tokens):
+            logits = model(input_ids) # todo: incorporate attn mask and handle unequal prompts in the full logic
+            last_logits = logits[:, -1, :] # (bs*k, vocab_size)
+            v = last_logits.size(-1)
+
+            log_probs = torch.log_softmax(last_logits, dim=-1)
+            log_probs = log_probs.view(bs, k, v)
+            candidate_scores = log_probs + beam_scores.unsqueeze(-1) # bs, k, v
+            candidate_scores = candidate_scores.view(bs, -1) # bs, k*v
+
+            # (bs, k)
+            top_scores, top_idx = torch.topk(candidate_scores, k=k, dim=-1) 
+            beam_idx = top_idx // v
+            token_idx = top_idx % v
+
+            # get the top beams from prev input_ids and append correct tokens
+            input_ids = input_ids.view(bs, k, -1)
+            gather_idx = beam_idx.unsqueeze(-1).expand(-1, -1, input_ids.size(-1))
+            input_ids = input_ids.gather(1, gather_idx) # (bs, k, seq_len)
+            input_ids = torch.cat([input_ids, token_idx.unsqueeze(-1)], dim=-1) # (bs, k, prev_seq_len+1)
+
+            beam_scores = top_scores
+            input_ids = input_ids.view(bs*k, -1)
+
+            # todo: check finished beams, store them separately
+
+    return input_ids.view(bs, k, -1), beam_scores
+            
